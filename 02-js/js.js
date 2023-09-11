3e0177abd1363242097e69ca83d82090b97eee8a
PPsom <j2su0218@gmail.com>
2023-09-11 22:50:24 +0900

    20230911

---
+++ 02-js/js.js
@@ -0,0 +1,7 @@
+var num = 0;
+function box_close() {
+  document.getElementById("box" + num).style.display = "none";
+}
+function box_open() {
+  document.getElementById("box" + num).style.display = "block";
+}
